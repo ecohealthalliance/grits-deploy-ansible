121d31e01f55a098d7c9c01ac816eba64fa5ab84
David Manthey <david.manthey@kitware.com>
2014-11-14 13:20:23 -0500

    Refactored how alerts are loaded.
    Previously, alerts were loaded such that some data was ignored and some alerts were stored multiple times.  When an alert is for multiple geographic places, it occurs multiple times in HealthMap.  We stored the alert with an internal name of (HealthMap Id)(4 digit number) where the 4 digit number was based on how it was grouped in the response from HealthMap.  If this internal name already existed, the later alert would replace an earlier one.
    
    I've changed this so that the alert is always stored with the internal name of (HealthMap Id)0000.  Within this record, all different place data is stored in the metadata under a 'places' list.  Arbitrarily, whichever place has the lowest place_id is stored in the main metadata.  The means that existing interfaces will continue to work with this data, even if they are not aware of the places list.
    
    When updating a data set that was generated in the old method, existing items are renamed or consolidated only if they are processed within the new feed.  If there was no item with an internal name of (HealthMap Id)0000 and there is an existing entry for the item, one of the existing entries is renamed to (HealthMap Id)0000.  All entries with the same HealthMap Id and a non-zero 4 digit suffix are removed.
    
    This requires a new version of the gritsSearch plugin to take advantage of the new data layout.
    
    Additionally, I added a timezone library function to ensure that the date on the alert was properly being stored or compared with UTC dates and times.

--- roles/girder/templates/healthMapGirder.py
+++ roles/girder/templates/healthMapGirder.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import dateutil.parser
+import dateutil.tz
 import datetime
 
 import pymongo
@@ -11,6 +12,61 @@ import requests
 
 import girder
 from girder.utility import server, model_importer
+
+
+TimeZoneStr = '''-12 Y
+-11 X NUT SST
+-10 W CKT HAST HST TAHT TKT
+-9 V AKST GAMT GIT HADT HNY
+-8 U AKDT CIST HAY HNP PST PT
+-7 T HAP HNR MST PDT
+-6 S CST EAST GALT HAR HNC MDT
+-5 R CDT COT EASST ECT EST ET HAC HNE PET
+-4 Q AST BOT CLT COST EDT FKT GYT HAE HNA PYT
+-3 P ADT ART BRT CLST FKST GFT HAA PMST PYST SRT UYT WGT
+-2 O BRST FNT PMDT UYST WGST
+-1 N AZOT CVT EGT
+0 Z EGST GMT UTC WET WT
+1 A CET DFT WAT WEDT WEST
+2 B CAT CEDT CEST EET SAST WAST
+3 C EAT EEDT EEST IDT MSK
+4 D AMT AZT GET GST KUYT MSD MUT RET SAMT SCT
+5 E AMST AQTT AZST HMT MAWT MVT PKT TFT TJT TMT UZT YEKT
+6 F ALMT BIOT BTT IOT KGT NOVT OMST YEKST
+7 G CXT DAVT HOVT ICT KRAT NOVST OMSST THA WIB
+8 H ACT AWST BDT BNT CAST HKT IRKT KRAST MYT PHT SGT ULAT WITA WST
+9 I AWDT IRKST JST KST PWT TLT WDT WIT YAKT
+10 K AEST ChST PGT VLAT YAKST YAPT
+11 L AEDT LHDT MAGT NCT PONT SBT VLAST VUT
+12 M ANAST ANAT FJT GILT MAGST MHT NZST PETST PETT TVT WFT
+13 FJST NZDT
+11.5 NFT
+10.5 ACDT LHST
+9.5 ACST
+6.5 CCT MMT
+5.75 NPT
+5.5 SLT
+4.5 AFT IRDT
+3.5 IRST
+-2.5 HAT NDT
+-3.5 HNT NST NT
+-4.5 HLV VET
+-9.5 MART MIT'''
+TimeZoneDict = {}
+
+def getTimeZoneDict():
+    """
+    Timezones are often stored as abreviations such as EST.  These are
+    ambiguous, but should still be handled.  See
+    http://stackoverflow.com/questions/1703546
+    :returns: a dictionary that can be passed to dateutil.parser.parse.
+    """
+    if not len(TimeZoneDict):
+        for tz_descr in map(str.split, TimeZoneStr.split('\n')):
+            tz_offset = int(float(tz_descr[0]) * 3600)
+            for tz_code in tz_descr[1:]:
+                TimeZoneDict[tz_code] = tz_offset
+    return TimeZoneDict
 
 
 def loadConfig():
@@ -46,6 +102,8 @@ def loadHMap(config, day):
     # flatten the records
     records = response.json()
     allAlerts = []
+    if not records:
+        return allAlerts
     for record in records:
         n = 0
         alerts = record.pop('alerts')
@@ -54,7 +112,9 @@ def loadHMap(config, day):
             id = '%s%04i' % (idFromURL(alert['link']), n)
             alert['id'] = id
             allAlerts.append(alert)
-            n = n + 1
+            # always use zero-index now.  The trailing zeros are unnecessary
+            # but keep more data with the previous loading method
+            # n = n + 1
     return allAlerts
 
 
@@ -71,7 +131,8 @@ def filterAlert(alert):
         alert['rating'] = int(float(alert['rating']['rating']))
     except Exception:
         alert['rating'] = -1
-    alert['date'] = dateutil.parser.parse(alert['formatted_date'])
+    alert['date'] = dateutil.parser.parse(alert['formatted_date'],
+                                          tzinfos=getTimeZoneDict())
     alert['updated'] = datetime.datetime.now()
     try:
         alert['lat'] = float(alert['lat'])
@@ -100,7 +161,6 @@ def setupGirder():
 
 
 def main(*args):
-
     # open config file
     config = loadConfig()
 
@@ -145,6 +205,8 @@ def main(*args):
 
     nAdded = 0
     nUpdated = 0
+    nIdentical = 0
+    nDeleted = 0
 
     # loop through all days in the range
     while start < end:
@@ -154,28 +216,63 @@ def main(*args):
 
         # loop through all alerts in the response
         for alert in alerts:
-
+            if not 'place_id' in alert:
+                continue
             # process the data
             alert = filterAlert(alert)
             meta = {
-                'latitude': alert.get('lat'),
-                'longitude': alert.get('lng'),
                 'date': alert.get('date'),
                 'feed': alert.get('feed'),
                 'link': alert.get('original_url', alert.get('link')),
+                'description': alert.get('descr', '')
+            }
+            place = {
+                'latitude': alert.get('lat'),
+                'longitude': alert.get('lng'),
                 'country': alert.get('country'),
                 'disease': alert.get('disease'),
                 'rating': alert.get('rating'),
                 'species': alert.get('species_name'),
-                'description': alert.get('descr', '')
+                'diseases': alert.get('diseases'),
+                'place_id': alert['place_id']
             }
             desc = alert.get('summary', alert.get('summary_en', ''))
+            if desc is None or desc.strip() == '':
+                if alert.get('summary_en', ''):
+                    desc = alert.get('summary_en', '')
+            if desc is None or desc.strip() == '':
+                for key in alert:
+                    if key.startswith('summary_') and alert[key] and alert[key].strip():
+                        desc = alert[key]
+                        break
+            if desc is None or desc.strip() == '':
+                desc = alert['id']
+            desc = desc.strip()
 
             # check if the item already exists
             items = girderSearch(model['item'], {
                 'name': str(alert['id']),
                 'folderId': folder['_id']
             })
+            difference = False
+
+            # delete old items that used a non-zero index.  If the item didn't
+            # already exist, rename the first of these to
+            # an old ite
+            oldItems = girderSearch(model['item'], {
+                'name': {'$regex': '^%s(?!0000$)....$' % (
+                         str(alert['id'])[:-4], )},
+                'folderId': folder['_id']
+            })
+            if len(oldItems) and not len(items):
+                items = [oldItems[0]]
+                items[0]['name'] = alert['id']
+                items[0]['lowerName'] = items[0]['name'].lower()
+                oldItems = oldItems[1:]
+                difference = True
+            for item in oldItems:
+                model['item'].remove(item)
+                nDeleted += 1
 
             if len(items) > 1:
                 print >> sys.stderr, \
@@ -188,9 +285,31 @@ def main(*args):
             if len(items) > 0:
                 # update item
                 item = items[0]
+                if not 'meta' in item:
+                    item['meta'] = {}
+                if 'date' in item['meta']:
+                    item['meta']['date'] = item['meta']['date'].replace(
+                        tzinfo=dateutil.tz.tzutc())
+                if item['description'] != desc:
+                    difference = True
+                for key in meta:
+                    if item['meta'].get(key, None) != meta[key]:
+                        difference = True
+                places = {placeIter['place_id']: placeIter for placeIter in
+                          item['meta'].get('places', [])}
+                if alert['place_id'] not in places:
+                    difference = True
+                if (not difference and places[alert['place_id']] != place):
+                    difference = True
+                if alert['place_id'] == item['meta'].get('place_id', None):
+                    for key in place:
+                        if item['meta'].get(key, None) != place[key]:
+                            difference = True
+                if not difference:
+                    nIdentical += 1
+                    continue
                 item['description'] = desc
                 nUpdated += 1
-
             else:
                 # create the item
                 item = model['item'].createItem(
@@ -200,14 +319,24 @@ def main(*args):
                     description=desc
                 )
                 nAdded += 1
-
+                places = {}
+            item['meta'] = item.get('meta', {})
+            places[alert['place_id']] = place
+            item['meta']['places'] = places.values()
+            minPlaceId = str(min([int(placeId) for placeId in places]))
+            if minPlaceId == alert['place_id']:
+                item['meta']['place_id'] = alert['place_id']
+                meta.update(place)
             # add/update metadata
-            item['meta'] = meta
+            item['meta'].update(meta)
             model['item'].save(item, validate=False)
 
         start = start + datetime.timedelta(1)
     print 'Added %i new items' % nAdded
     print 'Updated %i old items' % nUpdated
+    print '%i identical old items' % nIdentical
+    if nDeleted:
+        print 'Deleted %i items' % nDeleted
 
 if __name__ == '__main__':
     import sys
