3590c095520f60f05faa116256b6c64a85027c0c
Freddie Rosario <rosario@ecohealthalliance.org>
2015-12-15 10:52:21 -0500

    Update security.sh to remove AWS creds from grits_api config.py

--- security.sh
+++ security.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 #Purpose: This script is meant to run inside of a docker container in order to strip out sensitive info right before shipping off to digital infuzion
 
+#Remove AWS creds from grits-api config.py
+sed -i "s/aws_access_key \= '.*'//" /home/grits/grits-api/config.py
+sed -i "s/aws_secret_key \= '.*'//" /home/grits/grits-api/config.py
+
+
+
+
+
+
+
+
 #Get rid of SSH server and any keys
 rm -fr /root/.ssh
 rm -fr /home/grits/.ssh
