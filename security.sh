6c14202233b8f1971416da7737a7a1e69b696aad
Freddie Rosario <rosario@ecohealthalliance.org>
2015-12-15 11:34:37 -0500

    Remove bsve credentials and config info

--- security.sh
+++ security.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 #Purpose: This script is meant to run inside of a docker container in order to strip out sensitive info right before shipping off to digital infuzion
 
-#Remove AWS creds from grits-api config.py
+#Remove AWS creds from grits-api/config.py
 sed -i "s/aws_access_key \= '.*'//" /home/grits/grits-api/config.py
 sed -i "s/aws_secret_key \= '.*'//" /home/grits/grits-api/config.py
 
+#Remove BSVE creds/config info from grits-api/config.py
+sed -i "s/bsve_endpoint \= '.*'/bsve_endpoint \= ''/" /home/grits/grits-api/config.py
+sed -i "s/bsve_user_name \= '.*'/bsve_user_name = ''/" /home/grits/grits-api/config.py
+sed -i "s/bsve_api_key \= '.*'/bsve_api_key = ''/" /home/grits/grits-api/config.py
+sed -i "s/bsve_secret_key \= '.*'/bsve_secret_key = ''/" /home/grits/grits-api/config.py
 
 
 
@@ -12,7 +17,9 @@ sed -i "s/aws_secret_key \= '.*'//" /home/grits/grits-api/config.py
 
 
 
-#Get rid of SSH server and any keys
+
+
+#Get rid of SSH server and any keys (Must stay as last task)
 rm -fr /root/.ssh
 rm -fr /home/grits/.ssh
 apt-get -y purge openssh-server
